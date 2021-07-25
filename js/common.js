1279382cfb6259abbcf7e7b7bdf5828e4ec54e63
ITkah <ilatkacenko132@gmail.com>
2021-07-25 15:13:26 +0300

    asd

--- js/common.js
+++ js/common.js
@@ -1,7 +1,52 @@
+$(".show_detail").on("click", function(){
+    $(this).toggleClass("show_detail_active");
+    $(".casino_pay_item").toggleClass("casino_pay_item_active");
+});
+
+$(".fact_img_wrap p").on("click", function(){
+    $(".fact_img_wrap img").show();
+    $(this).remove();
+});
+
 $.fn.extend({
     toggleText: function(a, b) {
         return this.text(this.text() == b ? a : b);
     }
+});
+
+function makesvg(percentage, inner_text=""){
+
+  var abs_percentage = Math.abs(percentage).toString();
+  var percentage_str = percentage.toString();
+  var classes = ""
+
+ var svg = '<svg class="circle-chart" viewbox="0 0 33.83098862 33.83098862" xmlns="http://www.w3.org/2000/svg">'
+     + '<circle class="circle-chart__background" cx="16.9" cy="16.9" r="15.9" />'
+     + '<circle class="circle-chart__circle '+classes+'"'
+     + 'stroke-dasharray="'+ abs_percentage+',100"    cx="16.9" cy="16.9" r="15.9" />'
+     + '<g class="circle-chart__info">'
+     + '   <span class="circle-chart__percent" x="17.9" y="15.5">'+percentage_str+'</span>';
+  
+  svg += ' </g></svg>';
+  
+  return svg
+}
+
+(function( $ ) {
+
+    $.fn.circlechart = function() {
+        this.each(function() {
+            var percentage = $(this).data("percentage");
+            var inner_text = $(this).text();
+            $(this).html(makesvg(percentage, inner_text));
+        });
+        return this;
+    };
+
+}( jQuery ));
+
+$(function () {
+     $('.circlechart').circlechart();
 });
 
 $(".best_quick a").on("click", function(e){
